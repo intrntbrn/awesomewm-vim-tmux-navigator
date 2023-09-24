173d09693ac9c3551067a3a1d1c07e34b36f27c2
BZ <b@zimmer.bz>
2023-09-24 20:03:21 +0200

    deprecate tmux_focus.sh

--- tmux_focus.sh
+++ tmux_focus.sh
@@ -7,12 +7,18 @@ wm_focus() {
 }
 
 case "$dir" in
-	"left")
-		if [ "$(tmux display-message -p '#{pane_at_left}')" -ne 1 ]; then tmux select-pane -L; else wm_focus; fi ;;
-	"right")
-		if [ "$(tmux display-message -p '#{pane_at_right}')" -ne 1 ]; then tmux select-pane -R; else wm_focus; fi ;;
-	"up")
-		if [ "$(tmux display-message -p '#{pane_at_top}')" -ne 1 ]; then tmux select-pane -U; else wm_focus; fi ;;
-	"down")
-		if [ "$(tmux display-message -p '#{pane_at_bottom}')" -ne 1 ]; then tmux select-pane -D; else wm_focus; fi ;;
+"left")
+	if [ "$(tmux display-message -p '#{pane_at_left}')" -ne 1 ]; then tmux select-pane -L; else wm_focus; fi
+	;;
+"right")
+	if [ "$(tmux display-message -p '#{pane_at_right}')" -ne 1 ]; then tmux select-pane -R; else wm_focus; fi
+	;;
+"up")
+	if [ "$(tmux display-message -p '#{pane_at_top}')" -ne 1 ]; then tmux select-pane -U; else wm_focus; fi
+	;;
+"down")
+	if [ "$(tmux display-message -p '#{pane_at_bottom}')" -ne 1 ]; then tmux select-pane -D; else wm_focus; fi
+	;;
 esac
+
+awesome-client 'require("naughty").notify({title = "awesomewm-vim-tmux-navigator", text = "tmux_focus.sh is deprecated. Please update your tmux keybinds."})'
